746e97f8b119db372a7f22ff40ebb8162dbb710f
axelbol <axel_lp_alvarado@hotmail.com>
2024-08-15 00:24:54 -0400

    test to check products in stock

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,22 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Product;
+use App\Models\Retailer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_it_checks_stock_for_products_at_retailers(): void
+    {
+        $switch = Product::create(['name' => 'Nintendo']);
+
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
     }
 }
